bbc10b3555a785bb4b1e6b59dfe6e22b5c26e2af
wesleyxbz <wesleyxbzs@gmail.com>
2020-11-23 14:03:20 -0300

    Organizado estrutura de arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {HomeComponent} from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+    data: {
+      title: ''
+    }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    }
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: {
+      title: 'Page not-found'
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/form-sign-in/form-sign-in.module.ts
+++ src/app/components/form-sign-in/form-sign-in.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class ErrorsModule { }
+export class FormSignInModule { }
