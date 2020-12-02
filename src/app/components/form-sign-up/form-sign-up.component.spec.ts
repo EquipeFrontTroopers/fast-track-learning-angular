e98c19fe9fb6b6f9fee97ac79a5b18059870ff8e
wesleyxbz <wesleyxbzs@gmail.com>
2020-12-02 15:07:36 -0300

    signup-component-test/Angular - Teste finalizado

--- src/app/components/form-sign-up/form-sign-up.component.spec.ts
+++ src/app/components/form-sign-up/form-sign-up.component.spec.ts
@@ -1,41 +1,63 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {FormSignUpComponent} from './form-sign-up.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {AuthAppService} from '../../core/service/auth-app.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
 
 describe('O componente FormSignUpComponent', () => {
   let component: FormSignUpComponent;
   let fixture: ComponentFixture<FormSignUpComponent>;
+  let authAppService: AuthAppService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FormSignUpComponent],
-      imports: [RouterTestingModule, ReactiveFormsModule]
+      declarations: [
+        FormSignUpComponent
+      ],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        AngularFireModule.initializeApp(environment.firebase)
+      ],
+      providers: [
+        AuthAppService,
+        FormBuilder
+      ]
     })
       .compileComponents();
   });
 
   beforeEach(() => {
+    authAppService = TestBed.inject(AuthAppService);
+
     fixture = TestBed.createComponent(FormSignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('Deve ser instanciado', () => {
+  it('deve ser instanciado', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Cadastrar o usuário', () => {
-    const fakeBody = {
-      nome: 'Carlos Silveira',
-      nickname: 'cSilveira',
-      email: 'carlos.silveira@compasso.com.br',
-      senha: '21232f297a57a5a743894a0e4a801fc3'
-    };
+  it('chamar inputHasError', () => {
+    const spy = spyOn(component, 'inputHasError').and.returnValue(true);
+    const result = component.inputHasError('email');
+    expect(spy).toHaveBeenCalled();
+    expect(result).toBeTruthy();
+  });
 
-    // códigos para chamar service...
-    expect(true).toBeTruthy();
+  it('deve cadastrar o usuário', () => {
+    const spySubmit = spyOn(component, 'submit').and.returnValue(null);
+    component.submit();
+    expect(spySubmit).toHaveBeenCalled();
+
+    const spySignUp = spyOn(authAppService, 'SignUp').and.returnValue(null);
+    authAppService.SignUp('', '', '');
+    expect(spySignUp).toHaveBeenCalled();
   });
 
 });
